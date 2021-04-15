1219ee1bcb13f773de775336e6dcc03d3e2a056f
Sarat Duvvuri <sarat.duvvuri@disney.com>
2021-04-15 11:39:33 -0400

    new problem

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,34 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
-    // Complete the minimumBribes function below.
-    static void minimumBribes(int[] q) {
+	private static int n = 8;
+	private static String line = "1 2 3 5 4 6 7 8";
 
+	// Complete the minimumBribes function below.
+	static void minimumBribes(int[] q) {
+		int bribe = 0;
+		for (int i = 0; i < q.length; i++) {
+			if (q[i] > i + 1) {
+				bribe = bribe + (q[i] - i + 1);
+			}
+		}
 
-    }
+	}
 
-    private static final Scanner scanner = new Scanner(System.in);
+	
 
-    public static void main(String[] args) {
-        int t = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	public static void main(String[] args) {
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int[] q = new int[n];
 
-            int[] q = new int[n];
+		String[] qItems = line.split(" ");
+		for (int i = 0; i < n; i++) {
+			int qItem = Integer.parseInt(qItems[i]);
+			q[i] = qItem;
+		}
 
-            String[] qItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		minimumBribes(q);
 
-            for (int i = 0; i < n; i++) {
-                int qItem = Integer.parseInt(qItems[i]);
-                q[i] = qItem;
-            }
-
-            minimumBribes(q);
-        }
-
-        scanner.close();
-    }
+	}
 }
